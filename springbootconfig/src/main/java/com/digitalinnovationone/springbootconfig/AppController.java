46d368c352af91003ed0ca2527fa71278ff119de
André Meira <andremeirati@gmail.com>
2021-06-24 04:21:45 -0300

    Adicionado endpoint para passar variável de ambiente configurada no application.properties e também passada na execução da aplicação via terminal

--- springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
+++ springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
@@ -6,13 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
-	
+
 	@Value("${app.message}")
 	private String appMessage;
-	
+
+	@Value("${ENV_DB_URL:NENHUMA}")
+	private String dbEnvironmentVariable;
+
 	@GetMapping("/")
 	public String getAppMessage() {
 		return appMessage;
 	}
 	
+	@GetMapping("/envVariable")
+	public String getEnvironmentVariable() {
+		return "A seguinte variável de ambiente foi passada: " + dbEnvironmentVariable;
+	}
+
 }
