24b13d1c414b3dd662450f81f03cddf5e3834dcc
André Meira <andremeirati@gmail.com>
2021-06-24 01:05:14 -0300

    Criado projeto SpringBoot e também adicionado projeto Angular que também é parte deste bootcamp.

---
+++ springboot-helloworld/src/main/java/com/digitalinnovationone/springboothelloworld/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.digitalinnovationone.springboothelloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/")
+	public String helloSpringBoot() {
+		return "Hello from Spring Boot";
+	}
+
+}
